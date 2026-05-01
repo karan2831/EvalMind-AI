5e0604e3d1711aa57ea4e269b598a778caba2303
karan <karakprbodhgaya12@gmail.com>
2026-05-01 13:48:36 +0530

    feat: add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css';
 import type { Metadata } from 'next';
+import Script from "next/script";
 import PaymentListener from '@/app/components/PaymentListener';
 
 export const metadata: Metadata = {
@@ -14,8 +15,23 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <head>
-        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Inter:wght@300..700&display=swap" rel="stylesheet"/>
-        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet"/>
+        <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&family=Inter:wght@300..700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:wght,FILL@100..700,0..1&display=swap" rel="stylesheet" />
+
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-X6KYR46TNZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-X6KYR46TNZ');
+          `}
+        </Script>
       </head>
       <body className="antialiased">
         <PaymentListener />
---
+++ utils/analytics.js
@@ -0,0 +1,8 @@
+export const trackEvent = (action, category, label) => {
+  if (typeof window !== "undefined" && window.gtag) {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label,
+    });
+  }
+};
